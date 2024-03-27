275a258a3d9233250e84a674a04ed54726f56ce9
Muhammad Anas Jamshaid <145829950+muhammadanasjamshaid@users.noreply.github.com>
2024-03-28 03:20:28 +0500

    This is a simple atm machine function

---
+++ main.ts
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+
+let myBalance = 10000; // Dollor
+let myPin = 1234;
+
+let pinanswer = await inquirer.prompt([
+  {
+    name: "pin",
+    type: "number",
+    message: "Enter Your Pin",
+  },
+]);
+
+if (pinanswer.pin === myPin) {
+  console.log("Correct Pin Code.!!");
+
+  let operationans = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "Please Select Option",
+      type: "list",
+      choices: ["Withdraw", "Check Balance"],
+    },
+  ]);
+
+  // console.log(operationans);
+
+  if (operationans.operation === "Withdraw") {
+    let amountAns = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "Enter Your Amount",
+        type: "number",
+      },
+    ]);
+    myBalance -= amountAns.amount;
+    console.log("Your remaing Balane is : " + myBalance);
+  } else if (operationans.operation === "Check Balance") {
+    console.log("Your Current Balance is : " + myBalance);
+  }
+} else {
+  console.log("Incorrect Pin Code.!!");
+}

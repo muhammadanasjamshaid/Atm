c06d0d2574e686af7564b4b6a820e6da0153785f
Muhammad Anas Jamshaid <145829950+muhammadanasjamshaid@users.noreply.github.com>
2024-03-28 03:24:22 +0500

    file changes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import inquirer from "inquirer";
+#! /usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
 let myBalance = 10000; // Dollor
 let myPin = 1234;
-let pinanswer = await inquirer.prompt([
+let pinanswer = await inquirer_1.default.prompt([
     {
         name: "pin",
         type: "number",
@@ -10,7 +16,7 @@ let pinanswer = await inquirer.prompt([
 ]);
 if (pinanswer.pin === myPin) {
     console.log("Correct Pin Code.!!");
-    let operationans = await inquirer.prompt([
+    let operationans = await inquirer_1.default.prompt([
         {
             name: "operation",
             message: "Please Select Option",
@@ -20,7 +26,7 @@ if (pinanswer.pin === myPin) {
     ]);
     // console.log(operationans);
     if (operationans.operation === "Withdraw") {
-        let amountAns = await inquirer.prompt([
+        let amountAns = await inquirer_1.default.prompt([
             {
                 name: "amount",
                 message: "Enter Your Amount",
@@ -31,7 +37,7 @@ if (pinanswer.pin === myPin) {
         console.log("Your remaing Balane is : " + myBalance);
     }
     else if (operationans.operation === "Check Balance") {
-        console.log("Your Current Balance is :" + myBalance);
+        console.log("Your Current Balance is : " + myBalance);
     }
 }
 else {
